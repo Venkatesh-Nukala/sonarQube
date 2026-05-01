f0c7d4aa1b0835dccff28d7648efa2ce0ffee8bc
Venkatesh-Nukala <venkateshnukala46@gmail.com>
2026-05-01 14:35:54 +0530

    Delete __tests__ directory

--- __tests__/app.test.js
+++
@@ -1,9 +0,0 @@
-const greet = require('../app');
-
-test('should return guest when null', () => {
-    expect(greet(null)).toBe("Hello Guest");
-});
-
-test('should return name when provided', () => {
-    expect(greet("Venkat")).toBe("Hello Venkat");
-});
